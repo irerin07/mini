4cd7b5a8c98ac4fcf2793237d6249dcecc14ac18
irerin07 <counterchord89@gmail.com>
2020-06-21 15:56:02 +0900

    주석 및 필요없는 코드들 제거

--- src/main/java/com/example/miniproject/security/JWT/AuthEntryPointJwt.java
+++ src/main/java/com/example/miniproject/security/JWT/AuthEntryPointJwt.java
@@ -20,7 +20,6 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        System.out.println("commence");
         logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
